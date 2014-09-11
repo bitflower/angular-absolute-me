2897d1ba74c3719bc36ce813fb51963a7ed8d508
bitflower <touch@bitflower.net>
2014-09-12 00:25:27 +0200

    Added minified js

--- src/net.bitflower.angular.absoluteMe.js
+++ src/net.bitflower.angular.absoluteMe.js
@@ -6,9 +6,9 @@
 // <date>2014-09-11</date>
 // <version>0.0.1</version>
 
-"use strict";
-
 (function () {
+
+    'use strict';
 
     var moduleName = 'net.bitflower.angular.dom';
 
@@ -22,40 +22,40 @@
     // delay:   number = milliseconds after which the absolute sizes are applied
     // initial: false = don't turn sizes into absolute on first load
 
-    angular.module(moduleName).directive('bfAbsoluteMe', function ($window) {
+    angular.module(moduleName).directive('bfAbsoluteMe', ['$window', function ($window) {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
 
-                // timer that is going to be cancel while resizing
+                // timer that is going to be canceled while resizing
                 var timer;
                 var time = 500;
-                if (attrs.delay != undefined && attrs.delay != '') {
-                    time = parseInt(attrs.delay);
+                if (attrs.delay !== undefined && attrs.delay !== '') {
+                    time = parseInt(attrs.delay, 10);
                 }
 
                 var initial = true;
-                if (attrs.initial != undefined && attrs.initial == 'false') {
+                if (attrs.initial !== undefined && attrs.initial === 'false') {
                     initial = false;
                 }
 
                 // While resizing: let the grid system (Bootstrap, Foundation, etc ...) layout the control
                 // Width 
-                if (attrs.w != undefined && attrs.w != '') {
+                if (attrs.w !== undefined && attrs.w !== '') {
                     element[0].style.width = '';
                 }
-                if (attrs.h != undefined && attrs.h != '') {
+                if (attrs.h !== undefined && attrs.h !== '') {
                     element[0].style.height = '';
                 }
 
                 function resize() {
 
                     // Width 
-                    if (attrs.w != undefined && attrs.w != '') {
+                    if (attrs.w !== undefined && attrs.w !== '') {
                         element[0].style.width = element[0].offsetWidth + 'px';
                     }
                     // Height 
-                    if (attrs.h != undefined && attrs.h != '') {
+                    if (attrs.h !== undefined && attrs.h !== '') {
                         element[0].style.height = element[0].offsetHeight + 'px';
                     }
                 }
@@ -76,6 +76,6 @@
 
             }
         };
-    });
+    }]);
 
-})();
+}());
